90066b6bd9848212c99088b619d8295ff14fec2a
suman gupta <coolsumangupta@gmail.com>
2024-03-15 18:36:01 +0530

    template created

---
+++ template.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+
+project_name="mlproject"
+
+
+list_of_files=[
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion/__init__.py",
+    f"src/{project_name}/components/data_transformation/__init__.py",
+    f"src/{project_name}/components/model_trainer/__init__.py",
+    f"src/{project_name}/components/model_monitering/__init__.py",
+    f"src/{project_name}/pipelines/__init__.py",
+    f"src/{project_name}/pipelines/training_pipeline/__init__.py",
+    f"src/{project_name}/pipelines/prediction_pipeline/__init__.py",
+    f"src/{project_name}/exception.py",
+    f"src/{project_name}/logger.py",
+    f"src/{project_name}/utils.py",
+    "Dockerfile",
+
+
+    
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating Directory:{filedir} for the {filename}")
+
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} is already exist")
